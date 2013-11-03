5f8c3eb2e27e6855aedb01f04e87fd67018d71fd
wojciechbujakowski <wojciech.bujakowski@uj.edu.pl>
2013-11-03 18:47:08 +0100

    Signed-off-by: wojciechbujakowski <wojciech.bujakowski@uj.edu.pl>

---
+++ breaking/definition/admin.py
@@ -0,0 +1,13 @@
+from userprofile.models import UserProfile,UserMission,Mission,Base,Item,UserEquipment,Reward,Rewards,Chat,Team,UserTeam
+from django.contrib import admin
+admin.site.register(UserProfile)
+admin.site.register(UserMission)
+admin.site.register(Mission)
+admin.site.register(Base)
+admin.site.register(Item)
+admin.site.register(UserEquipment)
+admin.site.register(Reward)
+admin.site.register(Rewards)
+admin.site.register(Chat)
+admin.site.register(Team)
+admin.site.register(UserTeam)
