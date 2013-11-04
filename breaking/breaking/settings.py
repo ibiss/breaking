a58edba37a552d4df773af372737ad9a4c6e40c1
Dariusz Michalski <dariusz.michalski@uj.edu.pl>
2013-11-04 18:07:59 +0100

    Add templates directory

--- breaking/breaking/settings.py
+++ breaking/breaking/settings.py
@@ -1,4 +1,5 @@
 # Django settings for breaking project.
+import os
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -108,6 +109,7 @@ ROOT_URLCONF = 'breaking.urls'
 WSGI_APPLICATION = 'breaking.wsgi.application'
 
 TEMPLATE_DIRS = (
+	os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
