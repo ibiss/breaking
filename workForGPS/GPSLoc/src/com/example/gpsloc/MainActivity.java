2e2149b816046ad2d1dfd94916e59bb7044cdf54
Jakub Laskowski <jakublas92@gmail.com>
2013-12-09 19:05:06 +0100

    Add marker of 'your location'

--- workForGPS/GPSLoc/src/com/example/gpsloc/MainActivity.java
+++ workForGPS/GPSLoc/src/com/example/gpsloc/MainActivity.java
@@ -12,40 +12,69 @@ import android.view.Menu;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends FragmentActivity {
 	
 	private GoogleMap map;
+	private int userIcon, destinationIcon;
+	private LocationManager mlocManager;
+	private double lat,lng;
+	private Marker userMarker;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		userIcon = R.drawable.yellow_point;
+		destinationIcon = R.drawable.green_point;
 		
 		map = ((SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-		
-		LocationManager mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+		mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
 		LocationListener mlocListener = new MyLocationListener();
 
 		mlocManager.requestLocationUpdates( LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
 		
 	}
-
+	
+	private void updatePlaces()
+	{
+		Location lastLoc = mlocManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+		//lat = lastLoc.getLatitude();
+		//lng = lastLoc.getLongitude();
+		LatLng lastLatLng = new LatLng(lat, lng);
+		
+		if(userMarker!=null) userMarker.remove();
+		
+		userMarker = map.addMarker(new MarkerOptions()
+	    .position(lastLatLng)
+	    .title("You are here")
+	    .icon(BitmapDescriptorFactory.fromResource(userIcon))
+	    .snippet(""));
+		
+		map.animateCamera(CameraUpdateFactory.newLatLng(lastLatLng), 1000, null);
+	}
+	
 	public class MyLocationListener implements LocationListener
 	{
 
 		@Override
 		public void onLocationChanged(Location loc)
 		{
-			loc.getLatitude();
-			loc.getLongitude();
+			lat = loc.getLatitude();
+			lng = loc.getLongitude();
 			
 			String Text = "My current location is: " +"Latitud = " + loc.getLatitude() + "Longitud = " + loc.getLongitude();
-			
+			updatePlaces();
 			TextView gpsData = (TextView) findViewById(R.id.wspolrzedne);
 			gpsData.setText(Text);
 			
