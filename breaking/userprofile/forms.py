9219cf1ea0db8cd224d346ea4de5d1b6c9c2ad69
Dariusz Michalski <abdalakbar@gmail.com>
2013-12-11 12:07:47 +0100

    i02t07: correct register and usercreateform

--- breaking/userprofile/forms.py
+++ breaking/userprofile/forms.py
@@ -5,19 +5,19 @@ from userprofile.models import UserProfile
 import datetime
 
 class UserCreateForm(UserCreationForm):
-	first_name = forms.CharField(max_length=100, required=True, label='Podaj imie')
-	second_name = forms.CharField(max_length=100, required=True, label='Podaj nazwisko')
-	latitude = forms.CharField(max_length=50, label='latitude')
-	longitude = forms.CharField(max_length=50, label='longitude')
-	
-	class Meta:
-		model = User
-		fields = ("username","first_name","second_name","latitude","longitude",)
+    first_name = forms.CharField(max_length=100, required=True, label='Podaj imie')
+    second_name = forms.CharField(max_length=100, required=True, label='Podaj nazwisko')
+    latitude = forms.CharField(max_length=50, label='latitude')
+    longitude = forms.CharField(max_length=50, label='longitude')
+    
+    class Meta:
+        model = User
+        fields = ("username","first_name","second_name","latitude","longitude",)
 
-	def save(self, commit=True):
-		if not commit:
-			raise NotImplementedError("Can't create User and UserProfile without database save")
-		user = super(UserCreateForm, self).save(commit=True)
-		user_profile = UserProfile(user=user, first_name=first_name, second_name = second_name, status=True, latitude = latitude, longitude = longitude, timestamp=datetime.datetime.now())
-		user_profile.save()
-		return user, user_profile
+    def save(self, commit=True):
+        if not commit:
+            raise NotImplementedError("Can't create User and UserProfile without database save")
+        user = super(UserCreateForm, self).save(commit=True)
+        user_profile = UserProfile(user=user, first_name=self.cleaned_data['first_name'], second_name = self.cleaned_data['second_name'], status=True, latitude = self.cleaned_data['latitude'], longitude = self.cleaned_data['longitude'], timestamp=datetime.datetime.now())
+        user_profile.save()
+        return user, user_profile
